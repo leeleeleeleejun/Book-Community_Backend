af7944e5db649a1ff200672b90904476a9ed9200
leeleeleeleejun <wnstjr6293@naver.com>
2023-12-15 09:29:05 +0900

    fix: 새로고침 시 404에러 발생 라우팅 추가 수정

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import mongoose from "mongoose";
 import cors from "cors";
 import memoRoutes from "./routes/memoRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
-import path from "path";
 
 dotenv.config();
 
@@ -13,13 +12,6 @@ app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use("/userimg", express.static("uploads"));
-// 정적 파일 제공 (build 폴더 내부의 파일들을 클라이언트에게 제공)
-app.use(express.static(path.join(__dirname, "build")));
-
-// 모든 경로에 대해 index.html 반환
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
 
 mongoose.connect(process.env.DB_KEY, {
   useNewUrlParser: true,
